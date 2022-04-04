ae3bf5aea3f6015e96c340c3c53dd04666b18f16
angelmoya <amoyapardo@gmail.com>
2022-04-04 15:16:40 +0000

    ADD new module

---
+++ helpdek_angelmoya/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Helpdek Angel Moya',
+    'summary': 'Manage helpdesk tickets',
+    'version': '15.0.1.0.0',
+    'author': 'Angel Moya',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/helpdesk_ticket_views.xml',
+    ],
+}
