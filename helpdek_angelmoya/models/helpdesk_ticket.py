aa5f61bcc909422bef3a29ee4bf7a04e230166e7
angelmoya <amoyapardo@gmail.com>
2022-04-06 16:34:39 +0000

    ADD Task 01-03

--- helpdek_angelmoya/models/helpdesk_ticket.py
+++ helpdek_angelmoya/models/helpdesk_ticket.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields, models, api
 
 class HelpdeskTicket(models.Model):
     _name = "helpdesk.ticket"
@@ -6,7 +6,7 @@ class HelpdeskTicket(models.Model):
     _order = "sequence"
 
     name = fields.Char(required=True)
-    description = fields.Text()
+    description = fields.Text(translatable=True)
     date = fields.Date(help="Date when the ticket was created")
     limit_date = fields.Datetime(help="Date and time when the ticket will be closed")
     assigned= fields.Boolean(help="Ticket assigned to someone", readonly=True)
@@ -14,7 +14,22 @@ class HelpdeskTicket(models.Model):
     user_id = fields.Many2one(
         comodel_name='res.users',
         string='Assigned to')
+    partner_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Partner')
     sequence = fields.Integer()
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions Done')
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        # relation='table_name',
+        # column1='col_name',
+        # column2='other_col_name',
+        string='Tags')
+    
+    
     
     state = fields.Selection(
         [('nuevo', 'Nuevo'),
@@ -25,5 +40,27 @@ class HelpdeskTicket(models.Model):
          ('cancelado', 'Cancelado')],
         string='State',
         default='nuevo')
+
+    def to_asignado(self):
+        self.ensure_one()
+        self.state = 'asignado'
     
+    def to_en_proceso(self):
+        self.write({'state': 'en_proceso'})
     
+    def to_pendiente(self):
+        for record in self:
+            record.state = 'pendiente'
+    
+    def review_actions(self):
+        self.ensure_one()
+        self.action_ids.review()
+
+        # actions = self.env['helpdesk.ticket.action'].search([('ticket_id', '=', self.id)])
+        # actions.review()
+    
+    @api.model
+    def get_amount_tickets(self):
+        # Give amount of ticket for active user
+        return self.search_count([('user_id', '=', self.env.user.id)])
+        
---
+++ helpdek_angelmoya/models/helpdesk_ticket_action.py
@@ -0,0 +1,17 @@
+from odoo import fields,models
+
+class HelpdeskTicketAction(models.Model):
+    _name = "helpdesk.ticket.action"
+    _description = "Helpdesk Ticket Action"
+    _order = "sequence"
+
+    sequence = fields.Integer()
+    name = fields.Char(required=True)
+    description = fields.Text()
+    duration = fields.Float()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket')
+    
+    def review(self):
+        for record in self:
+            record.description = '%s\n%s' % (record.description, '- OK')
