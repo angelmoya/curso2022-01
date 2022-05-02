75e800ad873152043b9d5751f1eea059c4229e5d
angelmoya <amoyapardo@gmail.com>
2022-05-02 16:08:17 +0000

    IMP

--- helpdek_angelmoya/models/helpdesk_ticket_tag.py
+++ helpdek_angelmoya/models/helpdesk_ticket_tag.py
@@ -1,9 +1,15 @@
-from odoo import fields,models
+from odoo import fields,models,api,Command
 
 class HelpdeskTicketTag(models.Model):
     _name = "helpdesk.ticket.tag"
     _description = "Helpdesk Ticket Tag"
 
+    @api.model
+    def _get_default_tickets(self):
+        if self.env.context.get('active_model') == 'helpdesk.ticket':
+            return [Command.set(self.env.context.get('active_ids'))]
+        return False
+    
     name = fields.Char(required=True)
     description = fields.Text()
     ticket_ids = fields.Many2many(
@@ -11,4 +17,10 @@ class HelpdeskTicketTag(models.Model):
         relation='helpdesk_ticket_tag_rel',
         column1='tag_id',
         column2='ticket_id',
-        string='Tickets')
+        string='Tickets',
+        default=_get_default_tickets)
+    
+    @api.model
+    def _clean_unused_tags(self):
+        unused_tags = self.search([('ticket_ids', '=', False)])
+        unused_tags.unlink()
